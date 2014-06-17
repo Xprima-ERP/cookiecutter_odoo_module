13be4392810bddd331253597efb37242b9aa6a02
Sebastien Delisle <sdelisle.auto123@gmail.com>
2014-06-17 07:23:52 -0400

    initial commit

---
+++ {{cookiecutter.module_name}}/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# To view the official documentation: https://doc.openerp.com/trunk/server/03_module_dev_01/
+
+{'name': '{{cookiecutter.module_name}}',
+ 'version': '{{cookiecutter.module_version}}',
+ 'summary': '{{cookiecutter.module_summary}}',
+ 'description': '{{cookiecutter.module_description}}',
+ 'category': '{{cookiecutter.module_category}}',
+ 'author': '{{cookiecutter.module_author}}',
+ 'website': '{{cookiecutter.module_website}}',
+ 'license': '{{cookiecutter.module_license}}',
+ 'depends': [],
+ 'data': [],
+ 'demo': [],
+ 'installable': {{cookiecutter.module_installable}},
+ 'auto_install': {{cookiecutter.module_auto_install}},
+ 'application': {{cookiecutter.module_application}},
+}
